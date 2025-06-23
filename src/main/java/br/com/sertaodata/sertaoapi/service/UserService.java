9409fd1614782b2c0cd07689eb38aa9b72e15fd6
Lucas Albuquerque <luucas.albuquerque23@gmail.com>
2025-06-23 14:15:08 -0300

    Feat: create endpoints getUserTheId, getUser and deleteUserTheId

--- src/main/java/br/com/sertaodata/sertaoapi/service/UserService.java
+++ src/main/java/br/com/sertaodata/sertaoapi/service/UserService.java
@@ -2,12 +2,19 @@ package br.com.sertaodata.sertaoapi.service;
 
 import br.com.sertaodata.sertaoapi.dtos.CreateUserRequest;
 import br.com.sertaodata.sertaoapi.dtos.UserCreatedDTO;
+import br.com.sertaodata.sertaoapi.model.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface UserService {
 
     UserCreatedDTO createUser(CreateUserRequest userDto);
 
+    User getUserTheId(Long id);
 
+    List<User> getUser();
+
+    void deleteUsertheId(Long id);
 }
--- src/main/java/br/com/sertaodata/sertaoapi/service/UserServiceImpl.java
+++ src/main/java/br/com/sertaodata/sertaoapi/service/UserServiceImpl.java
@@ -5,9 +5,12 @@ import br.com.sertaodata.sertaoapi.dtos.UserCreatedDTO;
 import br.com.sertaodata.sertaoapi.model.User;
 import br.com.sertaodata.sertaoapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +32,20 @@ public class UserServiceImpl implements UserService{
         User userCreate = userRepository.save(user);
 
         return new UserCreatedDTO("Usuário criado com sucesso.", userCreate.getId());
+    }
+
+    @Override
+    public User getUserTheId(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT, "Cliente não encontrado"));
+    }
+
+    @Override
+    public List<User> getUser() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void deleteUsertheId(Long id) {
+        userRepository.deleteById(id);
     }
 }
